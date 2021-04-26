426609b0024d20497b13dabcff62dde567decccf
wesbosse <wes.bosse@gmail.com>
2021-04-25 17:49:42 -0700

    spaghetti

--- src/server/index.js
+++ src/server/index.js
@@ -44,6 +44,43 @@ var imageData = {
     ]
 }
 
+
+const updateOverlay = () => {
+    let url = 'https://cdn.shoplightspeed.com/shops/634891/files/32936232/obg-twitchoverlay1-layers-2102011822.png'
+    Jimp.read(url, (err, overlay) => {
+        if (err) throw err;
+        Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(font1 => {
+            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font2 => {
+                overlay.print(font1, 1550, 80, imageData.turn); // turn counter
+                overlay.print(font1, 1535, 272, imageData.players[0].army); // player 1 army
+                overlay.print(font2, 1550, 335, imageData.players[0].name + ' | ' + imageData.players[0].team); // player 1 name/team
+                if(imageData.players[0].score / 10 > 1) {
+                    overlay.print(font1, 1568, 375, imageData.players[0].score); // player 1 score
+                } else {
+                    overlay.print(font1, 1583, 375, imageData.players[0].score);
+                }
+                overlay.print(font1, 1698, 375, imageData.players[0].cp); // player 1 CP
+                overlay.print(font1, 1535, 492, imageData.players[1].army); // player 2 army
+                overlay.print(font2, 1550, 555, imageData.players[1].name + ' | ' + imageData.players[1].team); // player 1 name/team
+                if(imageData.players[1].score / 10 > 1) {
+                    overlay.print(font1, 1568, 595, imageData.players[1].score); // player 2 score
+                } else {
+                    overlay.print(font1, 1583, 595, imageData.players[1].score);
+                }
+                overlay.print(font1, 1698, 595, imageData.players[1].cp); // player 2 CP
+                return overlay
+            }).then(overlay => {
+                return overlay.write('./public/overlay-current.png'); // save
+            }).catch(err => {
+                console.error(err);
+            });
+        });
+    });
+
+    return 420;
+}
+
+
 const app = express();
 
 var dir = path.join(__dirname, 'public');
@@ -67,6 +104,7 @@ app.post('/api/player/:index', (req, res) => {
         ...imageData.players[req.params.index - 1],
         ...req.body
     }
+    updateOverlay();
     res.status(200).redirect('/')
 });
 
@@ -76,34 +114,13 @@ app.get('/api/game', (req, res) =>
 
 app.post('/api/turn', (req, res) => {
     imageData.turn = req.body.turn
+    updateOverlay();
     res.status(200).redirect('/')
 });
 
 app.get('/api/gameOverlay', (req, res) => {
-    let url = 'https://cdn.shoplightspeed.com/shops/634891/files/32936232/obg-twitchoverlay1-layers-2102011822.png'
-    Jimp.read(url, (err, overlay) => {
-        if (err) throw err;
-        Jimp.loadFont(Jimp.FONT_SANS_64_BLACK).then(font1 => {
-            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(font2 => {
-                overlay.print(font1, 1550, 80, imageData.turn); // turn counter
-                overlay.print(font1, 1535, 272, imageData.players[0].army); // player 1 army
-                overlay.print(font2, 1550, 335, imageData.players[0].name + ' | ' + imageData.players[0].team); // player 1 name/team
-                overlay.print(font1, 1560, 375, imageData.players[0].score); // player 1 score
-                overlay.print(font1, 1700, 375, imageData.players[0].cp); // player 1 CP
-                overlay.print(font1, 1535, 492, imageData.players[1].army); // player 2 army
-                overlay.print(font2, 1550, 555, imageData.players[1].name + ' | ' + imageData.players[1].team); // player 1 name/team
-                overlay.print(font1, 1560, 595, imageData.players[1].score); // player 2 score
-                overlay.print(font1, 1700, 595, imageData.players[1].cp); // player 2 CP
-                return overlay
-            }).then(overlay => {
-                return overlay.write('./public/overlay-current.png'); // save
-            }).catch(err => {
-                console.error(err);
-            });
-        });
-    });
-
-    res.status(200).send()
+ 
+    res.status(200).redirect('/public/overlay-current.png')
 });
 
 
